95d79c20630f36969a4540d3d14048c926cdecd4
Tao <werewaswas@live.com>
2018-07-12 20:08:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-
+ 
   def table_name_for_insert
     self.class.table_name
   end
@@ -57,7 +57,14 @@ class Song
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
-
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+   
+    DB[:conn].execute(sql)
+   
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 end
 
 
